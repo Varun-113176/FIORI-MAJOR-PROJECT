d9abe96d520df7befad7058b25237a20b1e69f79
VARUN KUMAR NUNE <y22cse113@chalapathiengg.ac.in>
2026-02-28 06:47:05 +0000

    initial commit

---
+++ project1/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.varun.project1.project1.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ project1/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/varun/project1/project1/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
